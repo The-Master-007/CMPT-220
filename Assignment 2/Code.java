2762f95f5f63b4c61d3527212b540ac62dab27d1
Max DeSimone <maxdesimone144@gmail.com>
2026-01-27 16:06:40 -0500

    Update and rename code.java to Code.java

---
+++ Assignment 2/Code.java
@@ -0,0 +1,19 @@
+//always start with importing our scanner so we can use it!
+import java.util.Scanner
+public class Code {
+// Max DeSimone - CS Major, possibly Cyber minor, IS/IT minors obv
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int score = 0;
+        String ans = "";
+
+        System.out.println("Welcome to my quiz. You will have 3 questions to answer.");
+
+        System.out.println("What state was I born in?");
+        ans = sc.nextLine();
+        if (ans.
+
+        sc.close();
+    }
+}
+
