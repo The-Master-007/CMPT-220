a734c82aa1f300b98bd87e4bc15616d61d3b6dde
Max DeSimone <maxdesimone144@gmail.com>
2026-01-27 11:26:20 -0500

    Completed homework
    Added user input for age and calculated the sum of two numbers.

--- Assignment 1/code.java
+++ Assignment 1/code.java
@@ -1,12 +1,24 @@
 //always start with importing our scanner so we can use it!
 import java.util.Scanner;
+
 public class Debug {
- public static void main(String[] args) {
- //finish this one for me by receiving and printing the user's age back to them
- System.out.println("How old are you? ");
- //can you do some math for me? take these variables and print out the sum of them!
- int num1 = 10;
- int num2 = 37;
- //?
- }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //finish this one for me by receiving and printing the user's age back to them
+        System.out.println("How old are you? ");
+        String age = sc.nextLine();
+        System.out.println("You are " + age + " years old.");
+        //can you do some math for me? take these variables and print out the sum of them!
+        int num1 = 10;
+        int num2 = 37;
+     
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum + ".");
+        
+        sc.close();
+        /* I have done things like this in the past, but you need to make sure the 
+        * scanner is using the System.in otherwise it won't work. Don't forget to close
+        * the scanner, althought im sure vscode does it automatically.
+         */
+    }
 }
