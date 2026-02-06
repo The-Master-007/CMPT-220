6d9665f316991317f69b53f6961090b3af3b400f
Max DeSimone <maxdesimone144@gmail.com>
2026-02-06 17:45:24 -0500

    Create Main class with main method and array
    Added a Main class with a main method and an integer array.

--- Assignment 4/Code.java
+++ Assignment 4/Code.java
@@ -1,1 +1,7 @@
-
+public class Main {
+ public static void main(String[] args) {
+   //Don't let the size of this scare you! no matter how big an array is, it all works the same!
+   int[] myArray = {10,3,295,38,20,3,4,267,2445,10,5566,87,93,17,10,2,87,267,3176,3,82};
+   //you cannot use the array util. Do this one by hand :(
+ }
+}
