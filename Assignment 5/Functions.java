1e3009b80c8b1da2f650835b8b3b34d32575cec2
Max DeSimone <maxdesimone144@gmail.com>
2026-03-28 16:45:18 -0400

    started

--- Assignment 5/Functions.java
+++ Assignment 5/Functions.java
@@ -1,1 +1,50 @@
+import java.util.Scanner;
 
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your password: ");
+        String password = scan.next();
+        System.out.println("Your password is " + passwordChecker(password));
+    }
+    
+    public static String passwordChecker(String pass)
+    {
+        boolean hasUpper = false;
+        boolean hasLower = false;
+        boolean hasNum = false;
+        boolean hasSpecial = false;
+        String specials = "!@#$%^&*";
+        
+        for (int i = 0; i < pass.length(); i++)
+        {
+            char c = pass.charAt(i);
+            if (Character.isUpperCase(c))
+                hasUpper = true;
+            if (Character.isLowerCase(c))
+                hasLower = true;
+            if (Character.isDigit(c))
+                hasNum = true;
+            if (specials.indexOf(c) != -1)
+                hasSpecial = true;
+        }
+        
+        int score = 0;
+        if (hasUpper)
+            score++;
+        if (hasLower)
+            score++;
+        if (hasNum)
+            score++;
+        if (hasSpecial)
+            score++;
+            
+        if (pass.length() >= 10 && score >= 3)
+            return "Strong";
+        if (pass.length() >= 8 && score == 2)
+            return "Decent";
+        if (pass.length() >= 6 && score == 1)
+            return "Bad";
+        return "Horrible";
+    }
+}
