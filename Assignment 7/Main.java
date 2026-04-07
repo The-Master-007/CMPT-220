750033f8f2ee2fcdb57b322113fb04b8541871dd
Max DeSimone <maxdesimone144@gmail.com>
2026-04-07 11:14:13 -0400

    Add main method for enemy and wizard demonstration
    Implement main method to showcase enemy and wizard interactions.

---
+++ Assignment 7/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //create a generic enemy and call its functions
+        Enemy e1 = new Enemy(100, 20);
+        e1.attack();
+
+        //create a fire and an ice wizard and call all functions
+        Wizard w1 = new Wizard(100, 15, "fire");
+        w1.attack();
+        w1.damageType();
+        Wizard w2 = new Wizard(20, 10, "ice");
+        w2.attack();
+        w2.damageType();
+        Wizard w3 = new Wizard(75, 25, "lightning");
+        w3.attack();
+        w3.damageType();
+        //create a goblin and call its functions
+        Goblin g1 = new Goblin(40, 8);
+        g1.attack();
+        System.out.println();
+        // Showcasing damage functions and killing
+        w1.dealDamage(w2);
+        w3.dealDamage(w2);
+    }
+}
